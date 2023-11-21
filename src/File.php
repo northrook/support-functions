90f622c63b4b71973480380999eaba0fe289fae7
Martin <hello@northrook.com>
2023-11-21 06:55:59 +0000

    Updated File, renamed File::mkdir to File::makeDirectory, and added File::put.

--- src/File.php
+++ src/File.php
@@ -12,8 +12,51 @@ abstract class File {
 		return file_get_contents( $path ) ?: $onError;
 	}
 	
+	public static function put( ?string $content, string $filename, int $flags = 0, bool $override = true ) : false | int {
+		
+		if ( is_null( $content ) ) return false;
+		
+		$filename = File::makeDirectory( $filename );
+		
+		if ( ! $filename ) return false;
+		if ( ! $override && file_exists( $filename ) ) return false;
+		
+		$status = file_put_contents( $filename, $content, $flags );
+		
+		if ( ! $status ) return false;
+		
+		return $status;
+		
+		// $path = Str::before( $path, '/' );
+		// Debug::print( $filename );
+		// $context = stream_context_create( $context );
+		// Log::notice( $context );
+		// header( 'Content-Type: text/css; charset=UTF-8' );
+		// $context = stream_context_create(
+		// 	[
+		// 		'http' => [
+		// 			'method'  => 'POST',
+		// 			'header'  => "Content-Type: application/json\r\n" .
+		// 			             "Accept: application/json\r\n" .
+		// 			             "X-Content-Type-Options: nosniff",
+		// 			'content' => $content,
+		// 			'timeout' => 60,
+		// 		],
+		// 	] );
+		// $content = utf8_encode( $content );
+		// var_dump( mb_detect_encoding( $content ), basename( $filename ) );
+		// header( 'X-Content-Type-Options: nosniff' );
+		
+		// var_dump( get_headers( Get::asUrl( $filename ), true ) );
+		
+		// if ( $status ) Log::info( 'Wrote file to disk: ' . basename( $filename ) );
+		// if ( ! $status ) Log::warning( 'Unable to write to disk: ' . basename( $filename ) );
+		
+		// return false;
+	}
+	
 	public static function path( string $path, ?string $onError = null ) : string {
-		return File::mkdir( path : $path );
+		return File::makeDirectory( path : $path );
 	}
 	
 	/**
@@ -22,7 +65,7 @@ abstract class File {
 	 *
 	 * @return string|null
 	 */
-	public static function mkdir( string $path, int $permissions = 0755 ) : ?string {
+	public static function makeDirectory( string $path, int $permissions = 0755 ) : ?string {
 		
 		$path	= Str::filepath( path : $path );
 		$dir	= File::directoryPath( $path );
