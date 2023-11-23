bb5f94f0285ec9bebebcf5020cc57d6b7f5a9bc1
Martin <hello@northrook.com>
2023-11-23 19:44:09 +0000

    Updated File, renamed several methods.

--- src/File.php
+++ src/File.php
@@ -5,60 +5,27 @@ namespace Northrook\Support;
 
 abstract class File {
 	
-	public static function get( string $path, string $onError = null ) : ?string {
+	public static function getContents( string $path, string $onError = null ) : ?string {
 		$path = Str::filepath( path : $path );
-		if ( ! file_exists( $path ) ) return $onError;
+		if ( ! file_exists( $path ) ) {
+			return $onError;
+		}
 		return file_get_contents( $path ) ?: $onError;
 	}
 	
-	public static function put( ?string $content, string $filename, int $flags = 0, bool $override = true ) : false | int {
+	public static function putContents( ?string $content, string $filename, int $flags = 0, bool $override = true ) : false | int {
 		
-		if ( is_null( $content ) ) return false;
+		if ( is_null( $content ) ) {
+			return false;
+		}
 		
 		$filename = File::makeDirectory( $filename );
 		
-		if ( ! $filename ) return false;
-		if ( ! $override && file_exists( $filename ) ) return false;
+		if ( ! $filename || ( ! $override && file_exists( $filename ) ) ) {
+			return false;
+		}
 		
-		$status = file_put_contents( $filename, $content, $flags );
-		
-		if ( ! $status ) return false;
-		
-		return $status;
-		
-		// $path = Str::before( $path, '/' );
-		// Debug::print( $filename );
-		// $context = stream_context_create( $context );
-		// Log::notice( $context );
-		// header( 'Content-Type: text/css; charset=UTF-8' );
-		// $context = stream_context_create(
-		// 	[
-		// 		'http' => [
-		// 			'method'  => 'POST',
-		// 			'header'  => "Content-Type: application/json\r\n" .
-		// 			             "Accept: application/json\r\n" .
-		// 			             "X-Content-Type-Options: nosniff",
-		// 			'content' => $content,
-		// 			'timeout' => 60,
-		// 		],
-		// 	] );
-		// $content = utf8_encode( $content );
-		// var_dump( mb_detect_encoding( $content ), basename( $filename ) );
-		// header( 'X-Content-Type-Options: nosniff' );
-		
-		// var_dump( get_headers( Get::asUrl( $filename ), true ) );
-		
-		// if ( $status ) Log::info( 'Wrote file to disk: ' . basename( $filename ) );
-		// if ( ! $status ) Log::warning( 'Unable to write to disk: ' . basename( $filename ) );
-		
-		// return false;
-	}
-	
-	public static function path( string $path, bool $create = false, ?string $onError = null ) : ?string {
-		if ( $create ) return File::makeDirectory( path : $path );
-		$path = Str::filepath( path : $path );
-		if ( ! file_exists( $path ) ) return $onError;
-		return $path;
+		return file_put_contents( $filename, $content, $flags ) ?: false;
 	}
 	
 	/**
@@ -70,10 +37,15 @@ abstract class File {
 	public static function makeDirectory( string $path, int $permissions = 0755 ) : ?string {
 		
 		$path	= Str::filepath( path : $path );
-		$dir	= File::directoryPath( $path );
+		$dir	= File::getDirectoryPath( $path );
 		
-		if ( File::pathTraversable( path : $path ) ) return null;
-		if ( file_exists( $path ) || is_dir( $dir ) ) return $path;
+		if ( File::isPathTraversable( path : $path ) ) {
+			return null;
+		}
+		
+		if ( file_exists( $path ) || is_dir( $dir ) ) {
+			return $path;
+		}
 		
 		if (
 			mkdir( $dir, $permissions, true ) ||
@@ -85,7 +57,7 @@ abstract class File {
 		return null;
 	}
 	
-	public static function pathTraversable( string $path ) : bool {
+	public static function isPathTraversable( string $path ) : bool {
 		return Str::contains(
 			string		: $path,
 			substrings	: [ ".", "../", ".." . DIRECTORY_SEPARATOR ],
@@ -93,8 +65,43 @@ abstract class File {
 		);
 	}
 	
-	public static function directoryPath( string $path ) : string {
-		$path = substr( $path, 0, (int) strrpos( $path, DIRECTORY_SEPARATOR ) );
+	public static function getPath( string $path, bool $create = false, ?string $onError = null ) : ?string {
+		if ( $create ) {
+			return File::makeDirectory( path : $path );
+		}
+		$path = Str::filepath( path : $path );
+		
+		return ! file_exists( $path ) ? $onError : $path;
+	}
+	
+	public static function getDirectoryPath( string $path ) : string {
+		$path = substr( $path, 0, (int) strrpos( Str::filepath( $path ), DIRECTORY_SEPARATOR ) );
 		return rtrim( $path, DIRECTORY_SEPARATOR ) . DIRECTORY_SEPARATOR;
+	}
+	
+	/** Return the file name from a path.
+	 *
+	 * * Omits the extension by default.
+	 *
+	 * @param string		$path			The path
+	 * @param bool<false>	$withExtension	Include the file extension
+	 *
+	 * @return string
+	 */
+	public static function getFileName( string $path, bool $withExtension = false ) : string {
+		
+		$name = basename( path : Str::filepath( $path ) );
+		
+		if ( $withExtension ) {
+			return $name;
+		}
+		
+		$hasExtension = strrpos( $name, '.' );
+		
+		if ( $hasExtension === false ) {
+			return $name;
+		}
+		
+		return substr( $name, 0, $hasExtension );
 	}
 }
