8d56151d381715c23fb3f99b30bd598834a13d25
Martin <hello@northrook.com>
2023-11-30 08:12:01 +0000

    Prototyping Config system.

--- src/Config.php
+++ src/Config.php
@@ -3,17 +3,37 @@
 namespace Northrook\Support;
 
 final class Config {
-	
+
     private static Config $config;
 
+    public readonly string $rootDir;
+    public readonly string $assetsDir;
+    public readonly string $cacheDir;
+
+    private array $settings = [
+        'cache' => [
+            'invalidateAll' => true,
+        ],
+    ];
+
     public function __construct(
-        public readonly string $rootDir,
-        public readonly string $assetsDir,
+        string $rootDir,
+        string $assetsDir,
+            ?string $cacheDir,
+            ?array $settings = null
     ) {
-        static::$config = $this;
+        $this->settings  = $settings ?? $this->settings;
+        $this->rootDir   = $rootDir;
+        $this->assetsDir = $assetsDir;
+        $this->cacheDir  = $cacheDir ?? $this->rootDir . '/cache';
+        static::$config  = $this;
     }
 
     public static function get(): Config {
         return static::$config;
+    }
+
+    public function cache(): object {
+        return (object) $this->settings['cache'];
     }
 }
--- src/HTML/Asset.php
+++ src/HTML/Asset.php
@@ -9,17 +9,58 @@ use Northrook\Support\Str;
 final class Asset {
 
     use ConfigParameters;
+    private const VERSION_PREFIX = '?v=';
 
-    public readonly string $path;
+    public readonly ?string $path;
+    public readonly string $url;
     public readonly string $type;
+    public readonly string $version;
 
-    public function __construct( string $path, ?string $type = 'auto'    ) {
-        $this->path = Str::filepath( $path, $this::config()->assetsDir );
-        $this->type = $this->assetType( $type );
+    public function __construct( string $path, ?string $type = 'auto' ) {
+        $this->path    = $this->assetPath( $path );
+        $this->type    = $this->assetType( $type );
+        $this->version = $this->assetVersion();
+        $this->url     = $this->assetUrl( true );
+    }
+
+    public function link( ?string $type = null ): string {
+        $type ??= [
+            'css' => 'stylesheet',
+            'js'  => 'script',
+        ][$this->type];
+
+        return "<link rel=\"$type\" href=\"$this->url\">";
+    }
+
+    private function assetPath( string $path ): string {
+        $providedPath = Str::filepath( $path, $this::config()->assetsDir );
+        if ( file_exists( $providedPath ) ) {
+            return $providedPath;
+        }
+
+        // @todo Throw an exception
+
+        return null;
+    }
+
+    private function assetUrl( bool $withVersion = false, bool $relative = false ): string {
+        $url = str_replace( $this::config()->rootDir, '', $this->path );
+
+        if ( $withVersion ) {
+            $url = $url . self::VERSION_PREFIX . $this->version;
+        }
+
+        return Str::start( $url, '\\' );
     }
 
     private function assetType( ?string $type = 'auto' ): string {
         return $type === 'auto' ? pathinfo( $this->path, PATHINFO_EXTENSION ) : $type;
+    }
+
+    private function assetVersion(): string {
+        $version = filemtime( $this->path );
+
+        return $version;
     }
 
 }
