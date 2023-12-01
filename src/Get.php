497ca8c6fefa171b8f5e4867e0c3720bd6649236
Martin <hello@northrook.com>
2023-12-01 18:02:14 +0000

    Updated Element and Element::extractAttributes.

---
+++ src/Get.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Northrook\Support;
+
+class Get {
+
+    use ConfigParameters;
+
+    /**
+     * @TODO [mid] Add $class support
+     *
+     *
+     * @param  string|null   $get
+     * @param  string|null   $pack
+     * @param  string|null   $class
+     * @param  float|null    $stroke
+     * @return string|null
+     */
+    public static function icon(
+            ?string $get,
+            ?string $pack = null,
+            ?string $class = null,
+            ?float $stroke = null,
+        bool $raw = false,
+    ): ?string {
+        if ( ! $get ) {
+            return null;
+        }
+
+        $get  = array_filter( explode( ':', $get ) );
+        $icon = [
+            'name' => $get[0] ?? null,
+            'pack' => $get[1] ?? $pack ?? 'lucide',
+        ];
+
+        $path = Str::filepath( Get::config()->iconsDir . $icon['pack'] . '/' . $icon['name'] . '.svg' );
+        if ( ! file_exists( $path ) ) {
+            return null;
+        }
+
+        $icon = file_get_contents( $path );
+        $stroke ??= 1.5;
+        $icon = preg_replace( '/ stroke-width=".*?"/', " stroke-width=\"$stroke\"", $icon );
+        if ( $raw ) {
+            return $icon;
+        }
+
+        return "<i class=\"icon\">$icon</i>";
+    }
+}
