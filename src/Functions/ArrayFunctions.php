ff6007e7a41b67c9de0f3c65391e72633db3b785
Martin <hello@northrook.com>
2024-02-11 23:35:57 +0000

    mend

--- src/Functions/ArrayFunctions.php
+++ src/Functions/ArrayFunctions.php
@@ -16,24 +16,19 @@ trait ArrayFunctions {
 			return false;
 		}
 
-		if ( is_string( $value ) ) {			
-			foreach ( $array as $item ) {
-				if ( $condition === 'contains' && strpos( $item, $value ) !== false ) {
-					return $item;
-				} elseif ( $condition === 'startsWith' && str_starts_with( $item, $value ) ) {
-					return $item;
-				} elseif ( $condition === 'endsWith' && str_ends_with( $item, $value ) ) {
-					return $item;
-				}
+		foreach ( $array as $item ) {
+			if ( $condition === 'contains' && strpos( $item, $value ) !== false ) {
+				return $item;
+			} elseif ( $condition === 'startsWith' && str_starts_with( $item, $value ) ) {
+				return $item;
+			} elseif ( $condition === 'endsWith' && str_ends_with( $item, $value ) ) {
+				return $item;
+			} else if ( $item === $value ) {
+				return $item;
 			}
 		}
 
-		// Check if array contains value of any other type
-		$containsOtherType = count( array_filter( $array, function ( $item ) use ( $value ) {
-			return $item !== $value;
-		} ) ) > 0;
-
-		return ! empty( $partialMatch ) || $containsOtherType;
+		return false;
 
 	}
 }
