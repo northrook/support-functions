cf69ef09131dbebdd2e876d6d4b3de04bc3ee87f
Martin <hello@northrook.com>
2024-02-28 12:37:12 +0000

    mend

--- src/Attribute/Development.php
+++ src/Attribute/Development.php
@@ -7,4 +7,8 @@ use Attribute;
 #[Attribute( Attribute::TARGET_CLASS | Attribute::TARGET_METHOD )]
 class Development
 {
+	public function __construct(
+		string $note,
+		string $untilVersion = '1.0.0',
+	) {}
 }
