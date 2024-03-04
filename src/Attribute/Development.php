39b253c7ac1459d3c1c09302ccfde6e2c612aabe
Martin <hello@northrook.com>
2024-03-04 07:29:45 +0000

    mend

--- src/Attribute/Development.php
+++ src/Attribute/Development.php
@@ -3,12 +3,22 @@
 namespace Northrook\Support\Attribute;
 
 use Attribute;
+use JetBrains\PhpStorm\ExpectedValues;
 
 #[Attribute( Attribute::TARGET_FUNCTION | Attribute::TARGET_METHOD | Attribute::TARGET_PARAMETER | Attribute::TARGET_PROPERTY )]
 class Development
 {
 	public function __construct(
-		string  $note,
-		?string $untilVersion = '1.0.0',
+		#[ExpectedValues( [
+			'pending', // Not started, but planned
+			'bug',     // Something is wrong, see note for details
+			'static',  // "It needs improvement, but is working well, and I'll maybe get back to it"
+			'MVP',     // All features are working, needs refactoring/review
+			'beta',    // Fully working, but not ready for production
+			'Done',    // Ready for production
+		] )]
+		string  $status,
+		?string $note = null,
+		?string $untilVersion = null,
 	) {}
 }
